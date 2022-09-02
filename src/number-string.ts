5a57784247e23b000a60d0e729810d0359898414
greenthinktank <radionomoney@gmail.com>
2022-09-02 22:43:45 +0900

    array型の説明のためのファイル

---
+++ src/number-string.ts
@@ -0,0 +1,17 @@
+export {};
+
+let year: number = 1976;
+// year = false;
+console.log(year);
+
+// 16進数
+let age: number = 0x2b;
+// 43
+
+console.log(age);
+
+
+let name:string = 'Ham';
+
+// name = 10;
+console.log(name);
