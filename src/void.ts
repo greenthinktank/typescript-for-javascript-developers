7606060bd30771f571aafd535c40ed43f7447886
greenthinktank <radionomoney@gmail.com>
2022-09-03 23:31:44 +0900

    literal型の説明追加

--- src/void.ts
+++ src/void.ts
@@ -1,8 +1,24 @@
 export {};
 
-function returnNothing(): void {
-  console.log('I do not return anything!');
-}
+// function returnNothing(): void {
+//   console.log('I do not return anything!');
+// }
 
-console.log(returnNothing());
+// console.log(returnNothing());
+
+
+// // 型エイリアス
+// type typeSample = {
+//   name: string;
+//   age: number;
+// }
+
+// const sample: typeSample = {
+//   name: 'Ham',
+//   age: 43
+// };
+
+let dayOfTheWeek: '日' | '月' | '火' | '水' | '木' |'金' = '日';
+
+dayOfTheWeek = '月';
 
